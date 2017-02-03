5f508f06e2b455d0978c7ad87bca94e763af4f33
jennifer <jenniferstrej@gmail.com>
2017-02-03 15:54:10 +0000

    arn corresponding jisc's account

--- lambdakinesis1mapping.tf
+++ lambdakinesis1mapping.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
     batch_size = 100
-    event_source_arn = "arn:aws:kinesis:us-east-1:109549055893:stream/MX-figshare-in-test"
+    event_source_arn = "arn:aws:kinesis:us-east-1:458323522494:stream/MX-figshare-in-test"
     enabled = true
-    function_name = "arn:aws:lambda:us-east-1:109549055893:function:figshare-worker"
+    function_name = "arn:aws:lambda:us-east-1:458323522494:function:figshare-worker"
     starting_position = "TRIM_HORIZON"
 }
