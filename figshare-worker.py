5e2fca2a1f756ac9d4082ac8f69db25c9f72b69f
jennifer <jenniferstrej@gmail.com>
2017-02-08 11:44:16 +0000

    some example with an external package

--- figshare-worker.py
+++ figshare-worker.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
 
 import base64
-import json
+import requests
 
 print('Loading function UPDATED!')
 
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
+    result = requests.get("https://www.google.co.uk/")
+    assert result.status_code != 500, "500 response from google!"
     for record in event['Records']:
         # Kinesis data is base64 encoded so decode here
         payload = base64.b64decode(record['kinesis']['data'])
