ecbc1722e6e5a5c417c4679f661e969fa94dd093
jennifer <jenniferstrej@gmail.com>
2017-02-08 12:33:21 +0000

    corrected wrong packaging

--- ci-package.sh
+++ ci-package.sh
@@ -3,14 +3,12 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-zip -9 figshare-worker.zip
+zip -9 -D figshare-worker.zip figshare-worker.py
 
 cd $VIRTUAL_ENV/lib/python2.7/site-packages
 
-zip -r9 figshare-worker.zip
+zip -r9 $VIRTUAL_ENV/../figshare-worker.zip .
 
 cd $VIRTUAL_ENV/..
-
-zip -g figshare-worker.zip figshare-worker.py
 
 
